f73822566da9abffb02c7d859aab6a28804a5a80
lfischer <lfischer@uoguelph.ca>
2023-01-25 13:50:55 -0500

    Creation and deletion scripts working

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from sql_client import SQLClient
 
 #Test connection to database - PASSED 
 sql_client = SQLClient()
+sql_client.create_tables()
--- sql_client.py
+++ sql_client.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import OperationalError
 
 DATABASE = "psqldb"
 USER = "postgres"
@@ -7,9 +8,9 @@ HOST = "database-1.c4b1rigueer1.us-east-1.rds.amazonaws.com"
 PORT = 5432
 
 class SQLClient:
+    #Connect to database
     def __init__(self):
         try:
-            #Connect to database
             self.db = psycopg2.connect(
                 database = DATABASE,
                 user = USER,
@@ -20,3 +21,35 @@ class SQLClient:
             print("Connection to PostgreSQL DB successful")
         except:
             print("Error occurred in connecting to the DB")
+
+    #Create student_info table
+    def create_tables(self):
+        create_script = '''
+                        CREATE TABLE IF NOT EXISTS student_info (
+                            student_id INTEGER,
+                            student_university VARCHAR(50),
+                            student_program VARCHAR(50),
+                            student_cumulative_gpa DECIMAL,
+                            student_job_found BOOLEAN,
+                            student_num_applications INTEGER,
+                            student_job_salary INTEGER,
+                            student_job_satisfaction INTEGER
+                        );'''
+        try:
+            with self.db.cursor() as curs:
+                curs.execute(create_script)
+                print("Create Table: SUCCESS")
+            self.db.commit()
+        except OperationalError as e:
+            print("Error in creating tables: "'{e}')
+
+    #Delete student_info table
+    def delete_tables(self):
+        create_script = '''DROP TABLE IF EXISTS student_info;'''
+        try:
+            with self.db.cursor() as curs:
+                curs.execute(create_script)
+                print("Delete Table: SUCCESS")
+            self.db.commit()
+        except OperationalError as e:
+            print("Error in deleting tables: "'{e}')
