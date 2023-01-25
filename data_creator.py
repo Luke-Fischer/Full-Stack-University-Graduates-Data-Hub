ef2eee22c587686752de44a0d3cd0c80dfae7b5a
lfischer <lfischer@uoguelph.ca>
2023-01-25 14:30:53 -0500

    Created a selected list of universities + programs Is subject to change

---
+++ data_creator.py
@@ -0,0 +1,8 @@
+class DataCreator:
+    UNI_LIST = ["Brock University", "Carleton University", "Dalhousie University", "Lakehead University", "McGill University", "McMaster University",
+                "Nipissing Univerisity", "Queen's University", "University of British Columbia", "Trent University", "University of Alberta", "University of Calgary"
+                "University of Guelph", "Universiy of Manitoba", "University of Toronto", "University of Waterloo", "Wilfrid Laurier University", "York University" ]
+    
+    PROG_LIST = ["Biology", "Human Nutrition", "Chemical Engineering", "Civil Engineering", "Electrical Engineering", "Mechanical Engineering", "Chemistry", "Physics",
+                 "Biomedical Science", "Architecture", "Child Studies", "Gender Studies", "Sociology", "Political Science", "Pyschology", "Computer Science", "Environmental Science",
+                 "Finance", "Accounting", "Economics", "Geography", "History", "Human Kinetics", "Computer Engineering", "Studio Art", "Music", "Creative Writing", "Philosophy"]
